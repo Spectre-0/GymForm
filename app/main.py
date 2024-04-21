456a764a6206c5ba35120940b4d53ba5040e4daf
Al Adnan Uddin <shuhanuddin6@gmail.com>
2024-04-21 01:21:18 +0100

    Remove unused code in form_analysis.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,20 @@
+import kivy
+import json
+from datetime import datetime
+
+
+from kivy.config import Config
+
+# Set the resizable option to 0 to disable window resizing
+Config.set('graphics', 'resizable', '0')
+# Set the desired fixed window size
+Config.set('graphics', 'width', '430')
+Config.set('graphics', 'height', str(int(430 * (16 / 9))))
+kivy.require('2.2.1')  # replace with your current kivy version !
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+import matplotlib.pyplot as plt
+from kivy_garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
 from kivy.uix.camera import Camera
 from kivy.uix.button import Button
 from kivy.uix.label import Label
@@ -10,6 +25,11 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.core.window import Window
 from kivy.uix.image import Image
 from kivy.uix.behaviors import ButtonBehavior
+import time
+from kivy.properties import NumericProperty
+from kivy.properties import StringProperty 
+import os
+
 
 import numpy as np
 
@@ -19,62 +39,36 @@ import mediapipe as mp
 class ImageButton(ButtonBehavior, Image):
     pass
 
+def format_workout_name(name):
+    # Converts snake_case to Title Case
+    return ' '.join(word.capitalize() for word in name.split('_'))
+
 # Define screens for the application
 # Define screens for the application
 # Define screens for the application
 class SelectWorkoutScreen(Screen):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        layout = GridLayout(cols=2, size_hint=(None, None), size=(400, 400), pos_hint={'center_x': 0.5, 'center_y': 0.5}, padding=10, spacing=10)
-        layout.bind(minimum_height=layout.setter('height'))
-
-        # Define the workout names and corresponding image paths
-        workouts = [
-            ('Dumbbell Curls', './images/dumbbell_curl.png'),
-            ('Dumbbell Shoulder Press', './images/dumbbell_shoulder_press.jpg'),
-            ('Dumbbell Bent Over Row', './images/dumbbell_bent_over_rows.png'),
-            ('Dumbbell Lateral Raise', './images/dumbbell_lateral_raise.png'),
-            ('Dumbbell Bench Press', './images/dumbbell_bench_press.png'),
-            ('Dumbbell Fly', './images/dumbbell_fly.png')
-        ]
-
-        # Create image buttons for each workout
-        for workout, image_path in workouts:
-            btn = ImageButton(source=image_path, size_hint=(None, None), size=(150, 150))
-            btn.bind(on_press=self.go_to_workout)
-            layout.add_widget(btn)
-
-        self.add_widget(layout)
-
     def go_to_workout(self, instance):
         workout_detail_screen = self.manager.get_screen('workout_detail')
         workout_detail_screen.current_workout = instance.source.split('/')[-1].split('.')[0]  # Extract workout name from image path
+        workout_detail_screen.current_workout =format_workout_name(workout_detail_screen.current_workout)
         self.manager.current = 'workout_detail'
 
 
 
 
-
 class WorkoutDetailScreen(Screen):
-    current_workout = None  # Attribute to keep track of the current workout
+    current_workout = StringProperty('')  # Attribute to keep track of the current workout
+    good_form_count = NumericProperty(0)  # Counter for "Good form" occurrences
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.layout = BoxLayout(orientation='vertical')
-        self.camera = Camera(index=0, resolution=(640, 480), play=True)
-        self.analyze_button = Button(text='Analyze', size_hint=(1, 0.1))
-        self.analyze_button.bind(on_press=self.toggle_analysis)
-        self.critiques_label = Label(text='Critiques will appear here', size_hint=(1, 0.1))
-
-        self.back_button = Button(text='Back', size_hint=(1, 0.1))
-        self.back_button.bind(on_press=self.go_back)
-        
-        
-        self.layout.add_widget(self.camera)
-        self.layout.add_widget(self.analyze_button)
-        self.layout.add_widget(self.back_button)
-        self.layout.add_widget(self.critiques_label)
-        
-        self.add_widget(self.layout)
+        self.workout_label = self.ids.workout_label
+        self.camera = self.ids.camera
+        self.analyze_button = self.ids.analyze_button
+        self.critiques_label = self.ids.critiques_label
+        self.back_button = self.ids.back_button
+        self.last_analysis_time = None
+        self.last_landmarks = None
 
         # Initialize MediaPipe Pose
         self.mp_pose = mp.solutions.pose
@@ -83,7 +77,18 @@ class WorkoutDetailScreen(Screen):
         self.analysis_active = False
         self.analysis_event = None
 
-    def toggle_analysis(self, instance):
+    def view_history(self):
+        if self.analysis_active:
+            self.toggle_analysis()
+        history_screen = self.manager.get_screen('history')
+        history_screen.create_graph(self.current_workout)
+        self.manager.current = 'history'
+    def format_workout_name(name):
+        # Converts snake_case to Title Case
+        return ' '.join(word.capitalize() for word in name.split('_'))
+
+
+    def toggle_analysis(self):
         if not self.analysis_active:
             # Start continuous analysis
             self.analysis_active = True
@@ -96,44 +101,106 @@ class WorkoutDetailScreen(Screen):
             if self.analysis_event:
                 self.analysis_event.cancel()
                 self.analysis_event = None
+            if self.good_form_count > 0:
+                self.save_workout_data()  # Save the workout data just before resetting
+            self.good_form_count = 0
+
+    def save_workout_data(self):
+        # Create an entry for the current workout and count
+        new_data = {
+            "count": self.good_form_count,
+            "datetime": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        }
+        
+        # File path for the JSON data
+        file_path = 'workout_data.json'
+        
+        # Check if the file exists and has content
+        if os.path.exists(file_path) and os.stat(file_path).st_size != 0:
+            # Read the existing data from the file
+            with open(file_path, 'r') as file:
+                try:
+                    data = json.load(file)
+                except json.JSONDecodeError:
+                    data = {}
+        else:
+            data = {}
+        
+        # Add the new data to the existing workout key or create a new key if it doesn't exist
+        if self.current_workout in data:
+            data[self.current_workout].append(new_data)
+        else:
+            data[self.current_workout] = [new_data]
+        
+        # Write the updated data back to the file
+        with open(file_path, 'w') as file:
+            json.dump(data, file, indent=4)
 
     def analyze_form(self, dt):
         frame = self.frame_from_camera()
         if frame is not None:
             results = self.pose.process(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
             if results.pose_landmarks:
-                # Draw the pose annotations on the frame
+                landmarks = [{'x': lm.x, 'y': lm.y, 'z': lm.z} for lm in results.pose_landmarks.landmark]
+
+                # Check if enough time has passed or if the movement is significant
+                current_time = time.time()
+                if self.last_analysis_time is None or current_time - self.last_analysis_time >= 0.5:  # Adjust the delay (in seconds) as needed
+                    if self.last_landmarks is None or self.has_significant_movement(landmarks, self.last_landmarks):
+                        # Check which workout is selected and analyze accordingly
+                        if self.current_workout == format_workout_name("dumbbell_shoulder_press"):
+                            critique = self.analyze_shoulder_press(landmarks)
+                            
+                        elif self.current_workout == format_workout_name("dumbbell_curl"):
+                            critique = self.analyze_dumbbell_curls(landmarks)
+ 
+                        elif self.current_workout == format_workout_name("dumbbell_bent_over_rows"):
+                            critique = self.analyze_bent_over_row(landmarks)
+                            
+                        elif self.current_workout == format_workout_name("dumbbell_lateral_raise"):
+                            critique = self.analyze_lateral_raise(landmarks)
+
+                            
+                        elif self.current_workout == format_workout_name("dumbbell_bench_press"):
+                            critique = self.analyze_bench_press(landmarks)
+                            
+                        elif self.current_workout == format_workout_name("dumbbell_fly"):
+                            critique = self.analyze_dumbbell_fly(landmarks)
+                            
+                        else:
+                            critique = "Select a workout to analyze your form."
+                            print(self.current_workout)
+                        
+
+                      
+                        self.critiques_label.text = critique
+                        if critique == "Good form!":
+                            self.good_form_count += 1
+                            print(self.current_workout)
+      
+                        self.last_analysis_time = current_time
+                        self.last_landmarks = landmarks
+
+                # Update the texture with the annotated image
                 annotated_image = frame.copy()
                 mp.solutions.drawing_utils.draw_landmarks(
-                    annotated_image, 
-                    results.pose_landmarks, 
+                    annotated_image,
+                    results.pose_landmarks,
                     self.mp_pose.POSE_CONNECTIONS
                 )
-                
-                # Convert landmarks to a more usable structure
-                landmarks = [{'x': lm.x, 'y': lm.y, 'z': lm.z} for lm in results.pose_landmarks.landmark]
-                
-                # Check which workout is selected and analyze accordingly
-                if self.current_workout == 'Dumbbell Shoulder Press':
-                    critique = self.analyze_shoulder_press(landmarks)
-                elif self.current_workout == 'Dumbbell Curls':
-                    critique = self.analyze_dumbbell_curls(landmarks)
-                elif self.current_workout == 'Dumbbell Bent Over Row':
-                    critique = self.analyze_bent_over_row(landmarks)
-                elif self.current_workout == 'Dumbbell Lateral Raise':
-                    critique = self.analyze_lateral_raise(landmarks)
-                elif self.current_workout == 'Dumbbell Bench Press':
-                    critique = self.analyze_bench_press(landmarks)
-                elif self.current_workout == 'Dumbbell Fly':
-                    critique = self.analyze_dumbbell_fly(landmarks)
-                else:
-                    critique = "Select a workout to analyze your form."
-                
-                self.critiques_label.text = critique
-                
-                # Update the texture with the annotated image
                 texture = self.frame_to_texture(annotated_image)
                 self.camera.texture = texture
+
+
+    def has_significant_movement(self, current_landmarks, previous_landmarks):
+        # Calculate the difference between the current and previous landmarks
+        # This is just an example; you may need to adjust the calculation based on your requirements
+        difference = sum(abs(current_landmarks[i]['x'] - previous_landmarks[i]['x']) + abs(current_landmarks[i]['y'] - previous_landmarks[i]['y']) for i in range(len(current_landmarks)))
+
+        # Define a threshold for significant movement
+        threshold = 0.1  # Adjust the threshold as needed
+
+        return difference > threshold
 
     def analyze_shoulder_press(self, landmarks):
         # Check vertical alignment of wrists over shoulders at the top of the press
@@ -199,7 +266,10 @@ class WorkoutDetailScreen(Screen):
             texture = self.frame_to_texture(frame)
             self.camera.texture = texture
 
-    def go_back(self, instance):
+    def go_back(self):
+        # If analysis is active, stop it before going back
+        if self.analysis_active:
+            self.toggle_analysis()
         self.manager.current = 'select_workout'
 
 
@@ -312,19 +382,83 @@ class WorkoutDetailScreen(Screen):
 
     def is_close_horizontally(self, wrist_left, wrist_right):
         return abs(wrist_left['x'] - wrist_right['x']) < 0.1  # Adjust the threshold as needed
+    
+    def go_to_workout(self, instance):
+        workout_name = instance.source.split('/')[-1].split('.')[0]
+        self.current_workout = format_workout_name(workout_name)
+        self.manager.current = 'workout_detail'
+    
+
+class HistoryScreen(Screen):
+    def __init__(self, **kwargs):
+        super(HistoryScreen, self).__init__(**kwargs)
+        layout = BoxLayout(orientation='vertical')
+        self.plot_area = BoxLayout(orientation='vertical', size_hint=(1, 0.9))
+        layout.add_widget(self.plot_area)
+        
+        back_button = Button(
+            text="Back",
+            size_hint=(1, 0.1),
+            on_press=self.go_back
+        )
+        layout.add_widget(back_button)
+        self.add_widget(layout)
+
+    def create_graph(self, workout_type):
+        # Clear the current widgets in plot_area
+        self.plot_area.clear_widgets()
+
+        # Read data from JSON file
+        try:
+            with open('workout_data.json', 'r') as file:
+                data = json.load(file)
+                workout_data = data.get(workout_type, [])
+        except Exception as e:
+            workout_data = []
+            print("Failed to read workout data:", e)
+
+        counts = [entry['count'] for entry in workout_data]
+        dates = [datetime.strptime(entry['datetime'], "%Y-%m-%d %H:%M:%S") for entry in workout_data]
+
+        # Set the figure size based on DPI and desired width in inches
+        dpi = 100  # This can be adjusted to your needs
+        figure_width_in_inches = self.width / dpi  # Use the screen's width
+        figure_height_in_inches = self.plot_area.height / dpi  # Use the plot area's height
+
+        plt.figure(figsize=(figure_width_in_inches, figure_height_in_inches), dpi=dpi)
+        plt.plot(dates, counts, 'bo-')
+        plt.xlabel('Date Time')
+        plt.ylabel('Good Reps count')
+        plt.title(f'Workout History for {workout_type}')
+        plt.gcf().autofmt_xdate()
+
+        plt.subplots_adjust(bottom=0.2, top=0.9)  
+
+        canvas = FigureCanvasKivyAgg(plt.gcf())
+        self.plot_area.add_widget(canvas)
+    def go_back(self, instance):
+        self.manager.current = 'workout_detail'
 
 class GymFormApp(App):
     def build(self):
-        self.sm = ScreenManager()
-        self.select_workout_screen = SelectWorkoutScreen(name='select_workout')
-        self.workout_detail_screen = WorkoutDetailScreen(name='workout_detail')
+        # Set the background color for the entire app to light grey
+        Window.clearcolor = (0.9, 0.9, 0.9, 1)  # RGB values for light grey
 
-        self.sm.add_widget(self.select_workout_screen)
-        self.sm.add_widget(self.workout_detail_screen)
+        # Assuming you want a width of 360 pixels (you can change this to your desired width)
+        width =430
+        # The height is determined by the 9:16 aspect ratio
+        height = int(width * (16 / 9))
+
+        # Set window size and make it non-resizable
+        Window.size = (width, height)
+        Config.set('graphics', 'resizable', False)
+
+        self.sm = ScreenManager()
+        self.sm.add_widget(SelectWorkoutScreen(name='select_workout'))
+        self.sm.add_widget(WorkoutDetailScreen(name='workout_detail'))
+        self.sm.add_widget(HistoryScreen(name='history'))
 
         return self.sm
-    
-
 
 if __name__ == '__main__':
     GymFormApp().run()
